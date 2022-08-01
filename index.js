7b6e25780665de28bab223c4d691887e4eb8ac0a
Lwebuga Charles <105212396+charzlwebz256@users.noreply.github.com>
2022-08-01 17:10:59 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+console.log("I am coding at SIR Centre");
+var moneyreceived = 25000;
+console.log(typeof moneyreceived);
+var moneyreceived = 35000;
+console.log(moneyreceived);
+let money = 25070;
+console.log(money)
+
+
+ // Exercise 1
+ // Answer 1
+ const loggeduser = "Charles";
+ console.log(loggeduser);
+
+// Answer 2
+const age = 10;
+console.log(age);
+// Answer 3
+if (age < 13){
+    alert("You are not eligibe for this service");
+} 
+else{
+    alert("Welcome" + loggeduser);
+}
+
+const mark = 50;
+    if (mark > 75) {
+        console.log("The grade is A");
+    }
+    else if(mark > 60){
+        console.log("The grade is B");
+    }
+    else if (mark >40){
+        console.log("The grade is C");
+    }
+    else if (mark >20){
+        console.log("The grade is D");
+    }
+    else if (mark >10){
+        console.log("The grade is F");
+    }
